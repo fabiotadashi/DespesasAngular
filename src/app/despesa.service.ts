63ed609b72699b001a8f1fadcbfdf82f30450cab
Fabio Tadashi Miyasato <miyasato.fabio@gmail.com>
2016-11-19 19:17:25 -0200

    Filtro por mês e totalizador

--- src/app/despesa.service.ts
+++ src/app/despesa.service.ts
@@ -26,6 +26,7 @@ export class DespesaService {
             }
         }
   }
+
   
     save(despesa: Despesa): void {
         switch(despesa.tipo)
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router }            from '@angular/router';
+import * as moment from 'moment';
 
 /* Serviços */
 import { DespesaService }         from '../despesa.service';
@@ -15,7 +16,9 @@ import { Despesa }                from '../despesa';
 export class ListComponent implements OnInit {
   despesas: Despesa[];
   invisiveis: boolean[]=[];
-  
+  months: string[]=[];
+  monthSelected: string;
+
   constructor(  private despesaService: DespesaService,
                 private router: Router) { }
 
@@ -24,11 +27,44 @@ export class ListComponent implements OnInit {
     for (let despesa of this.despesas){
         this.invisiveis[despesa.id] = false;
     }
+    var count = 0;
+    this.monthSelected = 'Todos'
+    this.months.push('Todos');
+    while (count < 12) {
+       this.months.push(moment().month(count++).format("MMMM"));
+    }
+  }
+
+  getTotalMesTexto(): string {
+    if(this.monthSelected == 'Todos'){
+      return;
+    }else{
+      return 'em '+this.monthSelected;
+    }
+  }
+
+  onChange(month) {
+    this.despesas = [];
+    let despesasList = this.despesaService.getDespesas();
+    for (var i=0; i < despesasList.length; i++){
+        if ( moment(despesasList[i].data).format("MMMM") == this.monthSelected || this.monthSelected == 'Todos')
+        {
+            this.despesas.push(despesasList[i]);
+        }
+    }
   }
 
   getDespesas(): void {
     this.despesas = this.despesaService.getDespesas();
   }
+
+  getTotalDespesas(): number {
+      let total = 0;
+      for (let despesa of this.despesas){
+        total += despesa.valor;
+      }
+      return total;
+    }
 
   add(): void {
     this.router.navigate(['/add']);
